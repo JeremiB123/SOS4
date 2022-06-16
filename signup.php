5eb5181661d41da1d882f11cf0bf4bb4196124b1
JeremiB123 <91541268+JeremiB123@users.noreply.github.com>
2022-06-16 16:30:50 +0200

    Login menu+Index

--- signup.php
+++ signup.php
@@ -33,54 +33,35 @@ session_start();
 
 
 <!DOCTYPE html>
-<html>
+<html lang="en" >
 <head>
-	<title>Signup</title>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link href="https://fonts.googleapis.com/css?family=Assistant:400,700" rel="stylesheet"><link rel="stylesheet" href="css/loginsignup.css">
+
 </head>
 <body>
+<!-- partial:index.partial.html -->
+<img src="img/NHL.jpg" width="150" height="100" />
+<section class='login' id='login'>
+  <div class='head'>
+  <h1 class='company'>Onderzoekend vermogen</h1>
+  </div>
+  <div class='form'>
+  
+  <form method="post">
+			<div style="font-size: 20px;margin: 10px;color: white;">Inschrijven</div><br>
+			<input class="text" type="text" name="user_name"><br><br>
+			<input class="password" type="password" name="password"><br><br>
 
-	<style type="text/css">
-	
-	#text{
+			<input id="button" class="btn-login" type="submit" value="Signup">
+            <a href="login.php" class='forgot'>Inloggen</a>
+            <br><br>
 
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style>
-
-	<div id="box">
-		
-		<form method="post">
-			<div style="font-size: 20px;margin: 10px;color: white;">Signup</div>
-
-			<input id="text" type="text" name="user_name"><br><br>
-			<input id="text" type="password" name="password"><br><br>
-
-			<input id="button" type="submit" value="Signup"><br><br>
-
-			<a href="login.php">Click to Login</a><br><br>
 		</form>
-	</div>
+  </div>
+</section>
+<!-- partial -->
+
 </body>
 </html>
